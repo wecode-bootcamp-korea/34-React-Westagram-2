3437ce82d6abe1c11cbf5884e7f26b24eedee981
dduddu92 <suyun107@gmail.com>
2022-06-10 16:56:09 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/suyeonPark/Login/Login.js
+++ src/pages/suyeonPark/Login/Login.js
@@ -1,20 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './login.scss';
 
 const Login = () => {
   const navigate = useNavigate();
-  const response = 'SUCCESS';
   const [id, setId] = useState('');
   const [pw, setPw] = useState('');
+  const [colorAbled, setColorAbled] = useState('#b2dffc');
+  const [disabled, setDisabled] = useState('disabled');
 
   const goToMain = () => {
-    if (response === 'SUCCESS') {
-      alert('환영합니다:)');
-      navigate('/main-yeon');
-    } else {
-      alert('돌아가주세요:(');
-    }
+    navigate('/main-yeon');
   };
   //최상위 class name을 컴포넌트 이름과 동일하게 해주면 스타일링 겹침을 줄일 수 있다.
 
@@ -25,6 +21,16 @@ const Login = () => {
   function handlePwInput(event) {
     setPw(event.target.value);
   }
+
+  const buttonColorChange = () => {
+    if (id.includes('@') && pw.length >= 5) {
+      setColorAbled('#0195f7');
+      setDisabled('');
+    } else {
+      setColorAbled('#b2dffc');
+      setDisabled('disabled');
+    }
+  };
 
   return (
     <div class="login">
@@ -38,6 +44,7 @@ const Login = () => {
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
               onChange={handleIdInput}
+              onKeyUp={buttonColorChange}
             />
             <input
               id="pw"
@@ -45,8 +52,15 @@ const Login = () => {
               type="password"
               placeholder="비밀번호"
               onChange={handlePwInput}
+              onKeyUp={buttonColorChange}
             />
-            <button id="logInBtn" type="button" onClick={goToMain}>
+            <button
+              id="logInBtn"
+              type="button"
+              onClick={goToMain}
+              style={{ backgroundColor: colorAbled }}
+              disabled={disabled}
+            >
               로그인
             </button>
             {/*버튼 타입을 꼭 지정해줄 것 */}
