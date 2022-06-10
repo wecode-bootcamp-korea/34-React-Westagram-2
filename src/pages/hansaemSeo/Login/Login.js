9bff0b43ec40e9659b871154b010ebebd4fa457c
shs <hs.seo@kisos.net>
2022-06-10 21:06:35 +0900

    add : Mission 1, Mission 2 - 사용자 입력 데이터 저장 및 로그인 버튼 활성화 하여 버튼 클릭시 메인 화면으로 넘어갈 수 있도록 작업

--- src/pages/hansaemSeo/Login/Login.js
+++ src/pages/hansaemSeo/Login/Login.js
@@ -1,7 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './login.scss';
-const Login = () => {
-  return <div>login</div>;
+
+const Login = props => {
+  const [loginAbled, setLoginAbled] = useState(true);
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleChangeInfo = () => {
+    if (id && password) {
+      setLoginAbled(false);
+    } else {
+      setLoginAbled(true);
+    }
+  };
+  const handleLogin = ev => {
+    ev.preventDefault();
+
+    if (!id.includes('@')) {
+      alert('전화번호 또는 이메일 형식을 확인해주세요.');
+      return;
+    } else if (password.length < 6) {
+      alert('비밀번호를 6자 이상 입력해주세요.');
+      return;
+    }
+    navigate('/main-saem');
+  };
+
+  const handleIdInputChange = ev => {
+    setId(ev.target.value);
+  };
+
+  const handlePasswordInputChange = ev => {
+    setPassword(ev.target.value);
+  };
+
+  return (
+    <div className="login">
+      <main className="login-wrapper">
+        <h1 className="login-title">Westagram</h1>
+        <form
+          className="login-form"
+          onChange={handleChangeInfo}
+          onSubmit={handleLogin}
+        >
+          <input
+            className="login-input login-id"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+            onChange={handleIdInputChange}
+            required
+            autoComplete="off"
+          />
+          <input
+            className="login-input login-pw"
+            placeholder="비밀번호"
+            onChange={handlePasswordInputChange}
+            required
+            type="password"
+            autoComplete="off"
+          />
+          <button className="login-button" disabled={loginAbled}>
+            로그인
+          </button>
+        </form>
+        <p className="login-help-text">비밀번호를 잊으셨나요?</p>
+        <script src="./src/login.js" />
+      </main>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/hansaemSeo/Main/Main.js
+++ src/pages/hansaemSeo/Main/Main.js
@@ -1,7 +1,132 @@
 import React from 'react';
+import Feeds from '../../../components/hansaemSeo/Feeds/Feeds';
+import Footer from '../../../components/hansaemSeo/Footer/Footer';
+import Header from '../../../components/hansaemSeo/Header/Header';
+import UserCard from '../../../components/hansaemSeo/UserCard/UserCard';
+import UserCards from '../../../components/hansaemSeo/UserCards/UserCards';
 import './main.scss';
-const Main = () => {
-  return <div>main</div>;
+
+const Main = props => {
+  const myInfo = {
+    userName: 'kor-sams-dev',
+    userImage: '/images/hansaemSeo/me-icon.jpg',
+    imageSize: 'normal',
+    userSubDescription: "hansam | it's me",
+  };
+  const storyUsers = [
+    {
+      userImage: '/images/hansaemSeo/feed-user1.jpg',
+      userName: '_yum_s',
+      userSubDescription: '16분 전',
+      isPinkRound: true,
+    },
+    {
+      userImage: '/images/hansaemSeo/feed-user2.jpg',
+      userName: 'drink_eat_drink',
+      userSubDescription: '3시간 전',
+      isPinkRound: true,
+    },
+    {
+      userImage: '/images/hansaemSeo/feed-user3.jpg',
+      userName: 'hyukyc',
+      userSubDescription: '20시간 전',
+      isPinkRound: true,
+    },
+    {
+      userImage: '/images/hansaemSeo/recomand-icon1.jpg',
+      userName: 'jminkeek',
+      userSubDescription: '2일 전',
+      isPinkRound: true,
+    },
+    {
+      userImage: '/images/hansaemSeo/recomand-icon2.jpg',
+      userName: '_yum_s2',
+      userSubDescription: '365일 전',
+      isPinkRound: true,
+    },
+  ];
+
+  const recomandUsers = [
+    {
+      userImage: '/images/hansaemSeo/recomand-icon1.jpg',
+      userName: 'joaaaaaaahy',
+      userSubDescription: '_legend_a님 외2 명이 ...',
+      isAbleFollow: true,
+    },
+    {
+      userImage: '/images/hansaemSeo/recomand-icon2.jpg',
+      userName: 'rampart91',
+      userSubDescription: 'ringo,in.seoul님 외 12..',
+      isAbleFollow: true,
+    },
+    {
+      userImage: '/images/hansaemSeo/recomand-icon3.jpg',
+      userName: 'shawnjjoo',
+      userSubDescription: 'jimmylee21212님 외 1...',
+      isAbleFollow: true,
+    },
+    {
+      userImage: '/images/hansaemSeo/feed-user2.jpg',
+      userName: 'shawnjjoow21',
+      userSubDescription: 'jimmylee21212님 외 1...',
+      isAbleFollow: true,
+    },
+  ];
+
+  const feeds = [
+    {
+      feedId: 1,
+      userInfo: {
+        userName: 'kor-sams-dev',
+        userImage: '/images/hansaemSeo/feed-user1.jpg',
+      },
+      feedImage: '/images/hansaemSeo/feed-image1.jpg',
+      feedLikeMessage: 'aineworld님 외 10명이 좋아합니다',
+      feedLikeUserImage: '/images/hansaemSeo/feed-comment.jpg',
+    },
+    {
+      feedId: 2,
+      userInfo: {
+        userName: 'feeddd2',
+        userImage: '/images/hansaemSeo/feed-user2.jpg',
+      },
+      feedImage: '/images/hansaemSeo/feed-image2.jpg',
+      feedLikeMessage: 'sdcaewc님 외 230명이 좋아합니다',
+      feedLikeUserImage: '/images/hansaemSeo/feed-image3.jpg',
+    },
+  ];
+  return (
+    <div className="main">
+      <Header />
+      <main className="main-feeds">
+        <Feeds feeds={feeds} />
+        <nav className="main-nav">
+          <div className="wrapper-user">
+            <UserCard userCard={myInfo} />
+          </div>
+          <div className="nav-wrapper-story round-border">
+            <div className="story-title-wrapper">
+              <span className="story-title">스토리</span>
+              <span className="story-all">모두 보기</span>
+            </div>
+            <div className="storys">
+              <UserCards userCards={storyUsers} />
+            </div>
+          </div>
+          <div className="nav-wrapper-recommand round-border">
+            <div className="recommand-title-wrapper">
+              <span className="recommand-title">회원님을 위한 추천</span>
+              <span className="recommand-all">모두 보기</span>
+            </div>
+            <div className="recommands">
+              <UserCards userCards={recomandUsers} />
+            </div>
+          </div>
+          <Footer />
+        </nav>
+      </main>
+    </div>
+  );
 };
 
 export default Main;
