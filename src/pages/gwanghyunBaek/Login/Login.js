6092a9e65c7f6cb20d212562fc342fadff5baa5c
bbbb <bgh9651@gmail.com>
2022-06-10 16:39:20 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/gwanghyunBaek/Login/Login.js
+++ src/pages/gwanghyunBaek/Login/Login.js
@@ -1,7 +1,61 @@
 import React from 'react';
 import './login.scss';
+import { Link, useNavigate } from 'react-router-dom';
+import '../../../styles/reset.scss';
+import { useState } from 'react';
+
 const Login = () => {
-  return <div>login</div>;
+  const [id, setId] = useState();
+  const [pw, setPw] = useState();
+  const [abled, setAbled] = useState(true);
+  const eventId = e => setId(e.target.value);
+  const eventPw = e => setPw(e.target.value);
+
+  const btnAbled = () => {
+    (id.includes('@') && pw.length) > 4 ? setAbled(false) : setAbled(true);
+  };
+
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <form className="insta">
+        <header className="title">westagram</header>
+        <input
+          onKeyUp={btnAbled}
+          className="userId"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+          onChange={eventId}
+          type="text"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+        />
+        <h1>{id}</h1>
+        <input
+          onKeyUp={btnAbled}
+          onChange={eventPw}
+          className="userPw"
+          type="password"
+          placeholder="비밀번호"
+        />{' '}
+        <h1>{pw}</h1>
+        <button
+          className="login_btn"
+          disabled={abled}
+          onClick={() => {
+            if (id.includes('@') && pw.length > 4) {
+              navigate('/main');
+            }
+          }}
+          type="button"
+        >
+          로그인
+        </button>
+        <a className="a_login" href="#">
+          비밀번호를 잊으셨나요?
+        </a>
+      </form>
+    </>
+  );
 };
 
 export default Login;
--- src/pages/gwanghyunBaek/Main/Main.js
+++ src/pages/gwanghyunBaek/Main/Main.js
@@ -1,7 +1,326 @@
-import React from 'react';
 import './main.scss';
+import React from 'react';
+import '../../../styles/reset.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import {
+  faBookmark,
+  faMagnifyingGlass,
+  faEllipsis,
+} from '@fortawesome/free-regular-svg-icons';
 const Main = () => {
-  return <div>main</div>;
+  return (
+    <>
+      <nav>
+        <div className="insta_icons">
+          <span className="icons_span">
+            <FontAwesomeIcon
+              icon={faInstagram}
+              className="fa-brands fa-instagram"
+            />
+          </span>
+          <span className="nav_title">westagram</span>
+        </div>
+        <div className="input_box">
+          <input className="nav_input" type="text" placeholder="검색" />
+          {/* <FontAwesomeIcon icon={faMagnifyingGlass} /> */}
+        </div>
+        <div className="nav_icons">
+          <img
+            className="nachimban"
+            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+            alt=""
+          />
+          <img
+            className="heart"
+            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+            alt=""
+          />
+          <img
+            className="user"
+            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
+            alt=""
+          />
+        </div>
+      </nav>
+      <div className="width_">
+        <main>
+          <div className="feeds">
+            <div className="article">
+              <div className="main_post">
+                <div className="main_profile">
+                  <div className="main_profile_name">
+                    <img
+                      className="main_profile_img"
+                      src="/images/gwanghyunBaek/me1.jpeg"
+                      alt=""
+                    />
+                    <div className="main_profile_id">
+                      <span className="first_id">bbbb_0221</span>
+                      <span className="second_id">bbb_0221</span>
+                    </div>
+                  </div>
+                  <span>{/* <FontAwesomeIcon icon={faEllipsis} /> */}</span>
+                </div>
+              </div>
+              <div className="main_img">
+                <img
+                  className="big_me"
+                  src="/images/gwanghyunBaek/me2.jpeg"
+                  alt=""
+                />
+              </div>
+              <div className="main_feeds">
+                <div className="feeds_option">
+                  <svg
+                    aria-label="좋아요"
+                    className="_8-yf5 "
+                    color="#262626"
+                    fill="#262626"
+                    height="24"
+                    role="img"
+                    viewBox="0 0 24 24"
+                    width="24"
+                  >
+                    <path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path>
+                  </svg>
+                  <svg
+                    aria-label="댓글 달기"
+                    className="_8-yf5 "
+                    color="#262626"
+                    fill="#262626"
+                    height="24"
+                    role="img"
+                    viewBox="0 0 24 24"
+                    width="24"
+                  >
+                    <path
+                      d="M20.656 17.008a9.993 9.993 0 10-3.59 3.615L22 22z"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                    ></path>
+                  </svg>
+                  <svg
+                    aria-label="게시물 공유"
+                    className="_8-yf5 "
+                    color="#262626"
+                    fill="#262626"
+                    height="24"
+                    role="img"
+                    viewBox="0 0 24 24"
+                    width="24"
+                  >
+                    <line
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      x1="22"
+                      x2="9.218"
+                      y1="3"
+                      y2="10.083"
+                    ></line>
+                    <polygon
+                      fill="none"
+                      points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334"
+                      stroke="currentColor"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                    ></polygon>
+                  </svg>
+                </div>
+                <FontAwesomeIcon icon={faBookmark} />
+              </div>
+              <div className="like_up">
+                <img
+                  className="like_man"
+                  src="/images/gwanghyunBaek/pic4.avif"
+                  alt=""
+                />
+                <span>bbbb_0221님 외 4명이 좋아합니다.</span>
+              </div>
+              <div className="feeds_post">
+                <span>bbbb_0221</span>
+                <span>여행가고싶어어어엉어어..</span>
+                <span className="after_view"> 더 보기</span>
+                <div className="hour_ago">
+                  <span>1시간 전</span>
+                </div>
+                <ul className="feeds_ul"></ul>
+              </div>
+              <div className="feeds_comment">
+                <textarea
+                  onKeyUp="enterkey()"
+                  placeholder="댓글 달기..."
+                  className="PUqUI Ypffh"
+                  autoComplete="off"
+                  autoCorrect="off"
+                ></textarea>
+                <button disabled type="button" className="comment_post">
+                  게시
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="main-right">
+            <div className="right-top">
+              <div className="main_profile_name right_profile_name">
+                <img
+                  className="right_profile_img"
+                  src="/images/gwanghyunBaek/me1.jpeg"
+                  alt=""
+                />
+                <div className="right_profile_id">
+                  <span className="first_id">bbbb_0221</span>
+                  <span className="second_id">bbb_0221</span>
+                </div>
+              </div>
+            </div>
+            <div className="right-middle1">
+              <div className="story">
+                <span>스토리</span>
+                <span>모두 보기</span>
+              </div>
+              <div className="story_profile">
+                <img
+                  className="story_img1"
+                  src="/images/gwanghyunBaek/hayeon1.jpeg"
+                  alt=""
+                />
+                <div className="story_name1">
+                  <span>hayeon</span>
+                  <span>2시간 전</span>
+                </div>
+              </div>
+              <div className="story_profile">
+                <img
+                  className="story_img1"
+                  src="/images/gwanghyunBaek/hayeon2.jpeg"
+                  alt=""
+                />
+                <div className="story_name1">
+                  <span>hayeon</span>
+                  <span>1시간 전</span>
+                </div>
+              </div>
+              <div className="story_profile">
+                <img
+                  className="story_img1"
+                  src="/images/gwanghyunBaek/hayeon3.jpeg"
+                  alt=""
+                />
+                <div className="story_name1">
+                  <span>hayeon</span>
+                  <span>30분 전</span>
+                </div>
+              </div>
+              <div className="story_profile">
+                <img
+                  className="story_img1"
+                  src="/images/gwanghyunBaek/hayeon4.jpeg"
+                  alt=""
+                />
+                <div className="story_name1">
+                  <span>hayeon</span>
+                  <span>10분 전</span>
+                </div>
+              </div>
+              <div className="story_profile">
+                <img
+                  className="story_img1"
+                  src="/images/gwanghyunBaek/hayeon1.jpeg"
+                  alt=""
+                />
+                <div className="story_name1">
+                  <span>hayeon</span>
+                  <span>2시간 전</span>
+                </div>
+              </div>
+              <div className="story_profile">
+                <img
+                  className="story_img1"
+                  src="/images/gwanghyunBaek/hayeon1.jpeg"
+                  alt=""
+                />
+                <div className="story_name1">
+                  <span>hayeon</span>
+                  <span>2시간 전</span>
+                </div>
+              </div>
+            </div>
+            <div className="right-bottom">
+              <div className="follow_top">
+                <span>회원님을 위한 추천</span>
+                <span>모두 보기</span>
+              </div>
+              <div className="follow_bottom">
+                <div className="story_follow">
+                  <img
+                    className="story_img_bottom"
+                    src="/images/gwanghyunBaek/hayeon1.jpeg"
+                    alt=""
+                  />
+                  <div className=" story_name_bottom">
+                    <span>hayeon</span>
+                    <span>회원님을 팔로우 합니다</span>
+                  </div>
+                </div>
+                <button>팔로우</button>
+              </div>
+              <div className="follow_bottom">
+                <div className="story_follow">
+                  <img
+                    className="story_img_bottom"
+                    src="/images/gwanghyunBaek/hayeon2.jpeg"
+                    alt=""
+                  />
+                  <div className=" story_name_bottom">
+                    <span>hayeon</span>
+                    <span>회원님을 팔로우 합니다</span>
+                  </div>
+                </div>
+                <button>팔로우</button>
+              </div>
+              <div className="follow_bottom">
+                <div className="story_follow">
+                  <img
+                    className="story_img_bottom"
+                    src="/images/gwanghyunBaek/hayeon3.jpeg"
+                    alt=""
+                  />
+                  <div className=" story_name_bottom">
+                    <span>hayeon</span>
+                    <span>회원님을 팔로우 합니다</span>
+                  </div>
+                </div>
+                <button>팔로우</button>
+              </div>
+            </div>
+            <div className="right_footer">
+              <div className="hyper">
+                <a href="#">소개</a>
+                <a href="#">도움말</a>
+                <a href="#">홍보 센터</a>
+                <a href="#">API</a>
+                <a href="#">채용 정보</a>
+                <a href="#">개인정보처리방침</a>
+                <a href="#">약관</a>
+                <a href="#">위치</a>
+                <a href="#">인기 계정</a>
+                <a href="#">해시태그</a>
+                <a href="#">언어</a>
+              </div>
+              <div className="instalink">
+                <address>© 2022 INSTAGRAM FROM META</address>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default Main;
