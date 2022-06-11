c0efa6972ab69e90f20c6cf09bb7569fb901358e
shs <hs.seo@kisos.net>
2022-06-11 18:40:58 +0900

    ADD : 사용자 검색기능 추가

--- src/components/hansaemSeo/Header/Header.jsx
+++ src/components/hansaemSeo/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Header.scss';
 import {
   faCompass,
@@ -8,8 +8,56 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { Link } from 'react-router-dom';
+import UserCard from '../UserCard/UserCard';
+
+const SEARCH_USER_LIST = [
+  {
+    id: 1,
+    userName: 'kor-sams-dev',
+    userImage: '/images/hansaemSeo/me-icon.jpg',
+    userSubDescription: "hansam | it's me",
+  },
+  {
+    userName: 'first-user',
+    userImage: '/images/hansaemSeo/feed-user1.jpg',
+    userSubDescription: "i'm first!!!!!!",
+  },
+  {
+    id: 2,
+    userName: 'second-user',
+    userImage: '/images/hansaemSeo/feed-user2.jpg',
+    userSubDescription: '내가 두번째다!',
+  },
+  {
+    id: 3,
+    userName: 'third-111',
+    userImage: '/images/hansaemSeo/feed-user3.jpg',
+    userSubDescription: 'ittttt so goood',
+  },
+  {
+    id: 4,
+    userName: 'third-111',
+    userImage: '/images/hansaemSeo/feed-user3.jpg',
+    userSubDescription: 'ittttt so goood',
+  },
+  {
+    id: 5,
+    userName: 'third-111',
+    userImage: '/images/hansaemSeo/feed-user3.jpg',
+    userSubDescription: 'ittttt so goood',
+  },
+];
 
 const Header = props => {
+  const [searchUserList, setSearchUserList] = useState(SEARCH_USER_LIST);
+  const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    setSearchUserList(
+      SEARCH_USER_LIST.filter(el => el.userName.includes(searchText))
+    );
+  }, [searchText]);
+
   return (
     <header className="header">
       <div className="logo">
@@ -19,7 +67,25 @@ const Header = props => {
         </Link>
       </div>
       <div className="wrapperSearch">
-        <input className="searchInput" placeholder="검색" />
+        <input
+          className="searchInput"
+          placeholder="검색"
+          value={searchText}
+          onChange={ev => {
+            setSearchText(ev.target.value);
+          }}
+        />
+        <div className="searchBoxWrapper">
+          <ul className="searchBox">
+            {searchUserList.map(user => {
+              return (
+                <li className="searchItem" key={user.id}>
+                  <UserCard userCard={user} />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
       <div className="wrapperMenus">
         <FontAwesomeIcon className="menu" icon={faCompass} />
